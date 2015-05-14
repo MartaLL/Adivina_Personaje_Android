bb63b1d94d82e4a646f325b23423a71a2cd693e2
MartaLL <m_a_r_t_a_1_9@hotmail.com>
2015-05-14 17:37:27 +0200

    Update Servidor.java
    New comments

--- P1S(views)/src/mll/proyecto/servidor/Servidor.java
+++ P1S(views)/src/mll/proyecto/servidor/Servidor.java
@@ -1,33 +1,45 @@
 package mll.proyecto.servidor;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Servidor {
 
-    private ServerSocket servidor;
-    private Hilo hilo[];
+	private ServerSocket servidor;
+	private Hilo hilo[];
 
-    public Servidor() {
-        try {
-        	//El Servidor a la espera en el puerto 2812
-            servidor = new ServerSocket(2812);
-            System.out.println("Esperando a jugador...");
-            //Ejecutamos los Hilos
-            hilo = new Hilo[100];
-            for (int i = 0; i < 100; i++) {
-                hilo[i] = new Hilo(servidor);
-                new Thread(hilo[i]).start();
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-    
-    public static void main(String[] args) {
-    	//Iniciamos el Servidor
-        Servidor s = new Servidor();
-    }
-    
+	/**
+	 * Constructor del Servidor que se encarga de inicializar los hilos y el Socket
+	 */
+	public Servidor() {
+		Logger.getLogger(getClass().getName()).log(Level.INFO, "-->>CREANDO SERVIDOR<<--");
+		try {
+			//El Servidor a la espera en el puerto 2812
+			servidor = new ServerSocket(2812);
+			Logger.getLogger(getClass().getName()).log(Level.INFO, "-->Esperando a jugador...");
+			//Ejecutamos los Hilos
+			Logger.getLogger(getClass().getName()).log(Level.INFO, "-->>CREANDO HILOS<<--");
+			hilo = new Hilo[100];
+			for (int i = 0; i < 100; i++) {
+				hilo[i] = new Hilo(servidor);
+//				new Thread(hilo[i]).start();
+				Thread thread = new Thread(hilo[i]);
+				thread.start();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Ejecución del servidor
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Servidor();
+	}
+
 }
